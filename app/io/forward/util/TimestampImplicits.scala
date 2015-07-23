07637056253b0a6a391cfbb05a900076252eaced
Owain Lewis <owain@owainlewis.com>
2015-07-23 15:24:47 +0100

    Update TimestampImplicits.scala

--- app/io/forward/util/TimestampImplicits.scala
+++ app/io/forward/util/TimestampImplicits.scala
@@ -5,8 +5,9 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 trait TimestampImplicits {
+  implicit val rds: Reads[Timestamp] = 
+    (__ \ "time").read[Long].map { long => new Timestamp(long) }
 
-  implicit val rds: Reads[Timestamp] = (__ \ "time").read[Long].map { long => new Timestamp(long) }
-
-  implicit val wrs: Writes[Timestamp] = (__ \ "time").write[Long].contramap { (a: Timestamp) => a.getTime }
+  implicit val wrs: Writes[Timestamp] = 
+    (__ \ "time").write[Long].contramap { (a: Timestamp) => a.getTime }
 }
