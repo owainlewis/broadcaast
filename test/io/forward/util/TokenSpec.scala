05c4d2aea35ed15aaa32941ca495384c7afa10fe
Owain Lewis <owain@owainlewis.com>
2016-07-05 09:14:45 +0100

    Update TokenSpec.scala

--- test/io/forward/util/TokenSpec.scala
+++ test/io/forward/util/TokenSpec.scala
@@ -3,9 +3,7 @@ package io.forward.util
 import org.specs2.mutable._
 
 class TokenSpec extends Specification {
-
   "A Token" should {
-
     "have a getRandomAccessToken method that returns a random access token for a given user ID" in {
       Token.randomAccessToken.isInstanceOf[String] mustEqual true
       Token.randomAccessToken.length mustEqual 32
