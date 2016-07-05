230de8feda8724006068c0ad2083709b64f58204
Owain Lewis <owain@owainlewis.com>
2016-07-05 09:15:46 +0100

    Update HTTPSRedirectFilter.scala

--- app/filters/HTTPSRedirectFilter.scala
+++ app/filters/HTTPSRedirectFilter.scala
@@ -7,20 +7,18 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 object HTTPSRedirectFilter extends Filter {
 
   private val oneDay = 86400
-
   /**
    * Force HTTPS redirect in AWS ELB
    */
   def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
     requestHeader.headers.get("x-forwarded-proto") match {
       case Some(header) =>
-        if ("https" == header) {
+        if ("https" == header)
           nextFilter(requestHeader).map { result =>
             result.withHeaders(("Strict-Transport-Security", s"max-age=$oneDay"))
           }
-        } else {
+        else
           Future.successful(Results.Redirect("https://" + requestHeader.host + requestHeader.uri, 301))
-        }
       case None => nextFilter(requestHeader)
     }
   }
