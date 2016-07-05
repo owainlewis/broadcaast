c2eea9145a3d89f5b7851952202e3b087ba59270
Owain Lewis <owain@owainlewis.com>
2016-07-05 09:18:17 +0100

    Update HumanDate.scala

--- app/io/forward/util/HumanDate.scala
+++ app/io/forward/util/HumanDate.scala
@@ -4,17 +4,10 @@ import org.joda.time._
 import org.joda.time.format.PeriodFormat
 
 object HumanDate {
-
-  /** Remove milliseconds */
+  
   private val periodType =
     PeriodType.standard().withMillisRemoved
 
-  /**
-   * Given a date time, returns the time ago in words i.e 2 minutes ago
-   *
-   * @param date A DateTime object
-   * @return S
-   */
   def timeAgoInWords(date: DateTime): Option[String] = {
     PeriodFormat.getDefault.print(new Period(date, new DateTime, periodType))
       .split(',').headOption flatMap (_.split("and").headOption) map (_ ++ " ago")
